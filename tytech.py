d1ddb2822a404de2ab42d1fffd9c6d984e69b292
TyB-Tech <93352444+TyB-Tech@users.noreply.github.com>
2021-11-18 23:16:02 +0000

    good job

--- tytech.py
+++ tytech.py
@@ -1,2 +1,14 @@
 print("Welcome to Earth")
-print("Hello Martin Nguyen")
+print("Hello world")
+
+a= input("What is your name? ")
+b= input("What is your age? ")
+c=input("How are you? ")
+
+print('Hello my name is '+ a)
+print('My age is ' + b)
+print('I am '+ c)
+
+
+
+
